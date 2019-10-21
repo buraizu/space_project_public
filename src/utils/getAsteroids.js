07af45bd513fd3f4c27f0968453547cfbb07cd1f
Bryce Eadie <bryce.eadie@gmail.com>
2019-10-21 01:52:53 -0700

    Modify log statement in getAsteroids

--- src/utils/getAsteroids.js
+++ src/utils/getAsteroids.js
@@ -3,7 +3,7 @@ const rp = require('request-promise')
 require('dotenv').config()
 
 const getAsteroids = (date, callback) => {
-    console.log(`Keys of process from getAsteroids: ${Object.keys(process)}`)
+    console.log(`Keys of process.env from getAsteroids: ${Object.keys(process.env)}`)
     const regex = /^\d{4}-{1}\d{2}-{1}\d{2}$/
 
     const options = {
